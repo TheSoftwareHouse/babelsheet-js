d37ea299f4f1d10637d0f1793a5ebb32ad9f3172
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2018-08-03 16:45:10 +0200

    Add data transformation to JSON

--- src/services/cli/container.ts
+++ src/services/cli/container.ts
@@ -5,7 +5,9 @@ import InEnvStorage from '../../infrastructure/storage/in-env';
 import GoogleAuth from '../../shared/google/auth';
 import GoogleSheets from '../../shared/google/sheets';
 import TokenStorage from '../../shared/token/token';
+import ToJsonTransformer from '../producer/transformer/to-json.transformer';
 import Formatter from './formater';
+import InFileStorage from '../../../build/infrastructure/storage/in-file.d';
 
 export default function createContainer(options?: ContainerOptions): AwilixContainer {
   const container = awilix.createContainer({
@@ -14,15 +16,17 @@ export default function createContainer(options?: ContainerOptions): AwilixConta
   });
 
   container.register({
+    formatter: awilix.asClass(Formatter),
     googleAuth: awilix.asClass(GoogleAuth),
     googleSheets: awilix.asClass(GoogleSheets),
+    jsonTransformer: awilix.asClass(ToJsonTransformer, { lifetime: awilix.Lifetime.SINGLETON }),
+    logger: awilix.asValue(winstonLogger),
     inEnvStorage: awilix.asClass(InEnvStorage, { lifetime: awilix.Lifetime.SINGLETON }),
+    inFileStorage: awilix.asClass(InFileStorage),
+    port: awilix.asValue(process.env.PORT || 3000),
     tokenStorage: awilix
       .asClass(TokenStorage)
       .inject(() => ({ storage: container.resolve<InEnvStorage>('inEnvStorage') })),
-    port: awilix.asValue(process.env.PORT || 3000),
-    logger: awilix.asValue(winstonLogger),
-    formatter: awilix.asClass(Formatter),
   });
 
   return container;
--- src/services/cli/index.ts
+++ src/services/cli/index.ts
@@ -3,8 +3,10 @@ import * as yargs from 'yargs';
 import { Options } from 'yargs';
 import { ILogger } from 'node-common';
 import createContainer from './container';
-import Formatter from './formater';
+//import Formatter from './formater';
 import GoogleSheets from '../../shared/google/sheets';
+import ITransformer from '../producer/transformer/transformer';
+//import IStorage from '../../../build/infrastructure/storage/storage';
 
 dotenv.config();
 
@@ -27,11 +29,15 @@ async function main() {
     describe: 'Format to export',
     type: 'string',
   };
+
   const argv = yargs.usage('Usage: $0 generate [-f "format"]').option('f', fOptions).argv;
 
   const spreadsheetData = await container.resolve<GoogleSheets>('googleSheets').fetchSpreadsheet();
 
-  container.resolve<Formatter>('formatter').format(argv.f);
+  const transformedData = await container.resolve<ITransformer>('jsonTransformer').transform(spreadsheetData);
+
+  //await container.resolve<IStorage>('inFileStorage').set(transformedData);
+  //container.resolve<Formatter>('formatter').format(argv.f);
 }
 
 main();

5136aade3ff7bfc655eddf07129d12aa98e0d784
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2018-08-16 09:42:15 +0200

    Add tests for key-generator

---
+++ src/shared/translations/translations.key-generator.spec.ts
@@ -0,0 +1,11 @@
+import TranslationsKeyGenerator from './translations.key-generator';
+
+describe('getExtension', () => {
+  it('does return proper key for given filters', () => {
+    const translationsKeyGenerator = new TranslationsKeyGenerator();
+
+    const result = translationsKeyGenerator.generateKey('x', ['test', 'test2', 'test3'], 'extension-xml');
+
+    expect(result).toBe('x-test,test2,test3-extension-xml');
+  });
+});

37f20439c379fd2975f3fe006ff3ed1df0091746
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2018-08-17 13:49:13 +0200

    Update spreadsheet-to-xml-transformer tests

--- src/shared/transformers/spreadsheet-to-xml.transformer.spec.ts
+++ src/shared/transformers/spreadsheet-to-xml.transformer.spec.ts
@@ -26,11 +26,26 @@ describe('SpreadsheetToXmlTransformer', () => {
     expect(result).toBeFalsy();
   });
 
-  it('does generate xml from spreadsheet', async () => {
+  it('does generate xml for given language from spreadsheet', async () => {
     const object = { test: ['test'] };
     const langCode = 'en_US';
 
     spreadsheetToXmlTransformer.transform(object, langCode);
+
+    expect(spreeadsheetToJson.transform).toBeCalledWith(object, langCode);
+    expect(jsonToXml.transform).toBeCalledWith('spreadsheet return');
+  });
+
+  it('does generate xml object with languages from spreadsheet', async () => {
+    const spreeadsheetToJson2: ITransformer = {
+      supports: type => false,
+      transform: jest.fn(() => ({ en: { test: 'test' } })),
+    };
+    const spreadsheetToXmlTransformer = new SpreadsheetToXmlTransformer(spreeadsheetToJson2, jsonToXml);
+    const object = { test: ['test'] };
+    const langCode = 'en_US';
+
+    spreadsheetToXmlTransformer.transform(object);
 
     expect(spreeadsheetToJson.transform).toBeCalledWith(object, langCode);
     expect(jsonToXml.transform).toBeCalledWith('spreadsheet return');

cf9fc64fdb678e82570b6c6baea2a867e8cf8cc3
Jakub Musik <jakub.musik@thesoftwarehouse.pl>
2018-10-26 10:28:34 +0200

    Add format selection to integration tests

--- src/services/cli/interpreter/interpreter.spec.ts
+++ src/services/cli/interpreter/interpreter.spec.ts
@@ -13,24 +13,12 @@ import FilesCreators from '../files-creators/files-creators';
 import FakeFilesCreators from '../../../tests/fakeFileCreators';
 import { getLoggerMock } from '../../../tests/loggerMock';
 
-const loggerMock = getLoggerMock();
-const containerTemp = createContainer();
-const fileCreators = containerTemp.resolve<FilesCreators>('filesCreators');
-
-const container = containerTemp.register({
-  googleSheets: awilix
-    .asClass(FakeGoogleSheets)
-    .inject(() => ({ returnData: spreadsheetData.multiRawSpreadsheetData })),
-  logger: awilix.asValue(loggerMock),
-});
-const snapsPath = path.join('src', 'services', 'cli', 'interpreter', 'tests');
-
-const remakeFolder = folder => {
-  fs.removeSync(folder);
-  fs.mkdirsSync(folder);
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
 };
-const getPath = relativePath => path.join(snapsPath, relativePath);
-const isUnixHiddenPath = function(path) {
+const isUnixHiddenPath = path => {
   return /(^|\/)\.[^\/\.]/g.test(path);
 };
 const reset = '\x1b[0m';
@@ -45,6 +33,19 @@ const getColor = difference => {
     return reset;
   }
 };
+
+const loggerMock = getLoggerMock();
+const containerTemp = createContainer();
+const fileCreators = containerTemp.resolve<FilesCreators>('filesCreators');
+
+const container = containerTemp.register({
+  googleSheets: awilix
+    .asClass(FakeGoogleSheets)
+    .inject(() => ({ returnData: spreadsheetData.multiRawSpreadsheetData })),
+  logger: awilix.asValue(loggerMock),
+});
+const snapsPath = path.join('src', 'services', 'cli', 'interpreter', 'tests');
+const supportedFormats = ['yml', 'android', 'ios', 'json', 'xlf'];
 
 // get directories list in /tests
 const directories = fs.readdirSync(snapsPath);
@@ -68,6 +69,7 @@ const scenarios = directories.reduce((accumulator, directory) => {
     {
       description: desc.description,
       params: desc.params,
+      format: desc.format,
       resultPath: path.join(snapsPath, directory, 'result'),
       snapPath: path.join(snapsPath, directory, 'snap'),
     },
@@ -85,55 +87,61 @@ describe('CLI interpreter', () => {
   afterAll(() => {
     // remove all results
     scenarios.forEach(scenario => {
-      remakeFolder(scenario.resultPath);
+      fs.removeSync(scenario.resultPath);
+      fs.mkdirsSync(scenario.resultPath);
     });
   });
   // for each scenario
   scenarios.forEach(scenario => {
-    it(scenario.description, async () => {
-      // alter arguments and paths
-      container.register({
-        shadowArgs: awilix.asValue(['generate', ...scenario.params]),
-        filesCreators: awilix.asClass(FakeFilesCreators).inject(() => ({
-          innerFilesCreators: fileCreators,
-          basePath: scenario.resultPath,
-        })),
+    it(scenario.description, async done => {
+      await asyncForEach(scenario.format ? [scenario.format] : supportedFormats, async format => {
+        const resultPathWithFormat = path.join(scenario.resultPath, format);
+        // alter arguments and paths
+        container.register({
+          shadowArgs: awilix.asValue(['generate', '-f', format, ...scenario.params]),
+          filesCreators: awilix.asClass(FakeFilesCreators).inject(() => ({
+            innerFilesCreators: fileCreators,
+            basePath: resultPathWithFormat,
+          })),
+        });
+        // interpret arguments
+        const interpreter = await container.resolve<Interpreter>('interpreter').interpret();
+        // check if snap folder exists, create it if it doesent and copy test contents. Dont copy if the folder exists, in case of more files than expected being generated.
+        const snapPathWithFormat = path.join(scenario.snapPath, format);
+        const snapshotFolderExists = fs.existsSync(snapPathWithFormat);
+        if (!snapshotFolderExists) {
+          fs.mkdirsSync(snapPathWithFormat);
+          fs.copySync(resultPathWithFormat, snapPathWithFormat);
+        } else {
+          const directoryDiff = await dircompare.compare(resultPathWithFormat, snapPathWithFormat, {
+            compareContent: true,
+          });
+
+          // deepcompare different files
+          const deepAnalysis = directoryDiff.diffSet.filter(element => element.state !== 'equal');
+          deepAnalysis.forEach(difference => {
+            // expect files to be in both directories
+            expect(difference.path1).toBeDefined();
+            expect(difference.name1).toBeDefined();
+            expect(difference.path2).toBeDefined();
+            expect(difference.name2).toBeDefined();
+
+            const resultFilePath = path.join(difference.path1, difference.name1);
+            const snapFilePath = path.join(difference.path2, difference.name2);
+            const oldFile = fs.readFileSync(snapFilePath, 'utf8');
+            const newFile = fs.readFileSync(resultFilePath, 'utf8');
+            const diff = jsdiff.diffChars(oldFile, newFile);
+
+            const colorOutput = diff.reduce((accumulator, current) => {
+              return `${accumulator}${getColor(current)}${current.value}`;
+            }, `${resultFilePath}:\n`);
+            // print formatted diffs
+            console.log(colorOutput, getColor({}));
+            expect(deepAnalysis.length).toBe(0);
+          });
+        }
       });
-      // interpret arguments
-      const interpreter = await container.resolve<Interpreter>('interpreter').interpret();
-
-      // check if snap folder exists, create it if it doesent and copy test contents. Dont copy if the folder exists, in case of more files than expected being generated.
-      const snapshotFolderExists = fs.existsSync(scenario.snapPath);
-      if (!snapshotFolderExists) {
-        fs.mkdirsSync(scenario.snapPath);
-        fs.copySync(scenario.resultPath, scenario.snapPath);
-      } else {
-        const directoryDiff = await dircompare.compare(scenario.resultPath, scenario.snapPath, {
-          compareContent: true,
-        });
-        // deepcompare different files
-        const deepAnalysis = directoryDiff.diffSet.filter(element => element.state !== 'equal');
-        deepAnalysis.forEach(difference => {
-          // expect files to be in both directories
-          expect(difference.path1).toBeDefined();
-          expect(difference.name1).toBeDefined();
-          expect(difference.path2).toBeDefined();
-          expect(difference.name2).toBeDefined();
-
-          const resultFilePath = path.join(difference.path1, difference.name1);
-          const snapFilePath = path.join(difference.path2, difference.name2);
-          const oldFile = fs.readFileSync(snapFilePath, 'utf8');
-          const newFile = fs.readFileSync(resultFilePath, 'utf8');
-          const diff = jsdiff.diffChars(oldFile, newFile);
-
-          const colorOutput = diff.reduce((accumulator, current) => {
-            return `${accumulator}${getColor(current)}${current.value}`;
-          }, `${resultFilePath}:\n`);
-          // print formatted diffs
-          console.log(colorOutput, getColor({}));
-          return expect(deepAnalysis.length).toBe(0);
-        });
-      }
+      done();
     });
   });
 });
